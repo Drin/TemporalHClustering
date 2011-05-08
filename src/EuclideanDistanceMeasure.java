b95bb58537f91af410ac74ec7237d6f47a59fc7d
Aldrin <drin@mini-drin.(none)>
2011-05-07 21:38:30 -0700

    added the last files necessary for compiling hierarchical clustering. These are still unmodified

---
+++ src/EuclideanDistanceMeasure.java
@@ -0,0 +1,6 @@
+public class EuclideanDistanceMeasure extends SquaredEuclideanDistanceMeasure {
+	@Override
+	public double evaluateDistance(double[] p1, double[] p2) {
+		return Math.sqrt(super.evaluateDistance(p1, p2));
+	}
+}
---
+++ src/SquaredEuclideanDistanceMeasure.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class SquaredEuclideanDistanceMeasure implements DistanceMeasure {
+	@Override
+	public double evaluateDistance(double[] p1, double[] p2) {
+		double acc = 0;
+		for (int i = 0; i < p1.length; i++)
+			acc += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+		return acc;
+	}
+
+	@Override
+	public void initialDataSweep(List<double[]> data) { }
+}
