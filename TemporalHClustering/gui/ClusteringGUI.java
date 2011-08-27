95d79fb6e8ff2169886b0247aa23c7d19f9b7bbd
Aldrin <DrinMontana@gmail.com>
2011-08-27 10:20:49 -0700

    committing files that are an attempt to give a gui to my clustering program. Hopefully I didn't accidentally commit .class files as well

---
+++ TemporalHClustering/gui/ClusteringGUI.java
@@ -0,0 +1,8 @@
+package TemporalHClustering.gui;
+
+public class ClusteringGUI {
+   public static void main(String[] args) {
+      MainWindow gui = new MainWindow();
+      gui.showWindow();
+   }
+}
---
+++ TemporalHClustering/gui/MainWindow.java
@@ -0,0 +1,94 @@
+package TemporalHClustering.gui;
+
+import TemporalHClustering.gui.listeners.clusterListeners.ClusterSingleListener;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class MainWindow extends JFrame {
+   private JMenuBar mainMenuBar;
+   
+   public MainWindow() {
+      super("E.coli Clustering");
+
+      mainMenuBar = new JMenuBar();
+      
+      //Menus that are on the Menu Bar
+      JMenu fileMenu = new JMenu("File");
+      JMenu clusterMenu = new JMenu("Cluster");
+      //JMenu pyroMenu = new JMenu("PyroSequencing");
+
+      //width is loosely calculated as number of characters in JMenu * 10
+      setSize(new Dimension(250, 100));
+       
+      //Items that will go into the File menu
+      JMenuItem importFile = new JMenuItem("View dendogram file");
+      JMenuItem saveFile = new JMenuItem("Save As...");
+      JMenuItem exitProgram = new JMenuItem("Exit");
+       
+      //Items that will go into the cluster menu
+      JMenuItem clusterSingle = new JMenuItem("E.coli data file");
+      JMenuItem clusterMany = new JMenuItem("E.coli data directory");
+       
+      //Listener for exit menu item
+      exitProgram.addActionListener(new ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent e) {
+            System.exit(0);
+         }
+      });
+       
+      //Listener for view dendogram menu item
+      /*
+      ComparePyrogramsListener libCompareListener = new ComparePyrogramsListener();
+      libCompareListener.setOwner(this);
+      libCompare.addActionListener(libCompareListener);
+      */
+
+      //Listener for save as menu item
+      /*
+      CompareXMLPyrogramsListener pyroCompareListener = new CompareXMLPyrogramsListener();
+      pyroCompareListener.setOwner(this);
+      pyroCompare.addActionListener(pyroCompareListener);
+      */
+       
+      //Listener for clusterSingle menu item
+      ClusterSingleListener clustSingleListener = new ClusterSingleListener(this);
+      clusterSingle.addActionListener(clustSingleListener);
+       
+      //Listener for clusterMany menu item
+      /*
+      DisplayXMLPyrogramListener pyroDisplayListener = new DisplayXMLPyrogramListener();
+      pyroDisplayListener.setOwner(this);
+      pyroDisplay.addActionListener(pyroDisplayListener);
+      */
+       
+      //adding Items to their Menus
+      fileMenu.add(importFile);
+      fileMenu.add(saveFile);
+      fileMenu.add(exitProgram);
+       
+      clusterMenu.add(clusterSingle);
+      clusterMenu.add(clusterMany);
+
+      //adding Menus to the MenuBar
+      mainMenuBar.add(fileMenu);
+      mainMenuBar.add(clusterMenu);
+       
+      setJMenuBar(mainMenuBar);
+      //setSize(1000, 760);
+      validate();
+   }
+   
+   public void showWindow() {
+      setVisible(true);
+      repaint();
+   }
+}
