63c1f6d820af85f93e434703675649c3aa50e20b
Aldrin Montana <DrinMontana@gmail.com>
2011-11-30 14:34:26 -0800

    Adding untracked files

---
+++ TemporalHClustering/dataStructures/TimePeriod.java
@@ -0,0 +1,10 @@
+package TemporalHClustering.dataStructures;
+
+/*
+ * This class represents a period of time corresponding to the experiment
+ * conditions. Typically this constitutes a day in which isolates were
+ * collected, however it may also be months or weeks.
+ */
+public class TimePeriod {
+
+}
